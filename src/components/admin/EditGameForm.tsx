8b04a1786043bc669f82146f7ed6eb3c43ee79b0
Grupo Manso <grupomanso@gmail.com>
2025-07-21 19:20:24 +0000

    ReferenceError: useMemo is not defined     at EditGameForm (https://6000

--- src/components/admin/EditGameForm.tsx
+++ src/components/admin/EditGameForm.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useMemo } from 'react';
 import { useRouter } from 'next/navigation';
 import { useForm, useFieldArray, Controller } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
