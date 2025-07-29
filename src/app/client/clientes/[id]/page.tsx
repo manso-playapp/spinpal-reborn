145181c7b8630547a17a9ec8678381c057b1154f
Grupo Manso <grupomanso@gmail.com>
2025-07-29 22:30:56 +0000

    de dice esto

--- src/app/client/clientes/[id]/page.tsx
+++ src/app/client/clientes/[id]/page.tsx
@@ -20,8 +20,8 @@ async function getGameData(id: string): Promise<{ name: string } | null> {
   return { name: data.name || 'Juego sin nombre' };
 }
 
-export default async function ClientCustomerListPage(props: { params: { id: string } }) {
-  const gameId = props.params.id;
+export default async function ClientCustomerListPage({ params }: { params: { id: string } }) {
+  const gameId = params.id;
   const game = await getGameData(gameId);
 
   if (!game) {
