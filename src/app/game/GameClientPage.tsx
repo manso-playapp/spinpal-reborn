fd17c8c65d302c8f618a09b79eaa293deef46fd1
manso-playapp <grupomanso@gmail.com>
2025-08-22 10:40:48 -0300

    video no requerido

--- src/app/game/GameClientPage.tsx
+++ src/app/game/GameClientPage.tsx
@@ -19,7 +19,7 @@ interface GameData extends DocumentData {
   status: string;
   segments: any[];
   backgroundImage: string;
-  backgroundVideo: string;
+  backgroundVideo?: string;
   backgroundFit: string;
   qrCodeScale: number;
   rouletteScale: number;
